4303221d9bfaed26fdc0ab348433c91a3d7d98e2
Yan Kurbatov <deknowny@gmail.com>
2021-11-18 19:51:29 +0300

    Update examples

--- examples/sync.py
+++ examples/sync.py
@@ -6,8 +6,8 @@ import demapi
 base_path = pathlib.Path("assets")
 conf = demapi.Configure(
     base_photo=base_path / "example_source.png",
-    title="Yo",
-    explanation="demotivator!!!!"
+    title="ДАВАЙ ДАВАЙ УРААА",
+    explanation="Еще одна бесполезная либа для бесполезного языка"
 )
 image = conf.download()
 image.save(base_path / "example.png")
